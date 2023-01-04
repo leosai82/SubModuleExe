1dd8a7b2287d16386e4ec430a209f1de8d1f7a62
sahee <sahee@kakao.com>
2023-01-04 16:49:10 +0900

    no message

---
+++ SubModuleExe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SubModuleExe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new SubModuleLib.Class1();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
